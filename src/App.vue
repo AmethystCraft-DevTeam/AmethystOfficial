c13ca328d7b2bf1653cdf7eecf55c306935771d6
顾雨辰 <starryskyworld@guyuchendeMac-mini.local>
2025-09-24 01:17:59 +0800

    backup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import LiquidBackground from '@/components/LiquidBackground.vue'
+</script>
+
+<template>
+  <div class="relative h-96 w-full overflow-hidden rounded">
+    <LiquidBackground />
+  </div>
+</template>
